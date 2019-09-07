bc51964cc580d094145b134632c566194c973f56
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2019-09-06 20:18:00 -0400

    added budgetCard

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import BudgetCard from './components/BudgetCard';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is the app</h1>
+      <BudgetCard />
+    </div>
+  );
+}
+
+export default App;
