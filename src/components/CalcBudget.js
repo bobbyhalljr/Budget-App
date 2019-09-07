2f64d494b03f11ecf01ef3e1dc4fbdc9d62d2382
Bobby Liberti <BobbyLiberti@Mes-iMac.local>
2019-09-06 20:54:52 -0400

    added components and skeleton of app

---
+++ src/components/CalcBudget.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const CalcBudget = () => {
+
+    return (
+        <div>
+            <table>
+                <th className='income'>
+                    <h2>Income</h2>
+                    <tr>
+                        <td>Web Project + $2,500</td>
+                        <td>Logo design + $500</td>
+                        <td>Danielle</td>
+                    </tr>
+                </th>
+                </table>
+                <table>
+                <th className='expenses'>
+                    <h2>Expenses</h2>
+                    <tr>
+                        <td>Web Project + $2,500</td>
+                        <td>Logo design + $500</td>
+                        <td>Danielle</td>
+                    </tr>
+                </th>
+            </table>
+        </div>
+    )
+}
+
+export default CalcBudget;
